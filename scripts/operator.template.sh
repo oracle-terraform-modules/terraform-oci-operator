276bf1202b3d83b1244453826026911f0e703942
Ali Mukadam <ali.mukadam@oracle.com>
2021-03-23 21:44:25 +1100

    added improvement in operator package update so the operator host is available faster (#29)

--- scripts/operator.template.sh
+++ scripts/operator.template.sh
@@ -4,11 +4,13 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 if [ ${ol} = 8 ]; then
-  dnf -y upgrade --security
+  dnf makecache
+  dnf config-manager --enable ol8_developer && dnf -y install python3-oci-cli
 else
   yum -y -t update --security
+  yum install -y python3
+  pip3 install oci-cli
 fi
 
 sed -i -e "s/autoinstall\s=\sno/autoinstall = yes/g" /etc/uptrack/uptrack.conf
 uptrack-upgrade -y
-pip3 install oci-cli
