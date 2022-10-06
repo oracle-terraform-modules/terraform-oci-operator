93ea8ab1561452e6ccb523ab59004a64df6f6716
Devon Crouse <devon.crouse@oracle.com>
2022-10-05 21:38:04 -0600

    Use stable name for dynamic group (#69)
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>
    
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- instance_principal.tf
+++ instance_principal.tf
@@ -1,29 +1,43 @@
-# Copyright 2017, 2021 Oracle Corporation and/or affiliates.  All rights reserved.
+# Copyright (c) 2022, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-resource "oci_identity_dynamic_group" "enable_operator_instance_principal" {
+resource "random_id" "dynamic_group_suffix" {
+  keepers = {
+    # Generate a new suffix only when variables are changed
+    label_prefix = local.dynamic_group_prefix
+    tenancy_id   = var.tenancy_id
+  }
+
+  byte_length = 8
+}
+
+resource "oci_identity_dynamic_group" "operator_group" {
   provider = oci.home
 
-  compartment_id = var.tenancy_id
-  description    = var.label_prefix == "none" ? "dynamic group to allow operator instance to invoke services" : "dynamic group with label ${var.label_prefix} to allow operator to invoke services"
+  compartment_id = random_id.dynamic_group_suffix.keepers.tenancy_id
+  description    = "dynamic group %{ if var.label_prefix != "none" }with label ${var.label_prefix}%{ endif } to allow operator to invoke services"
 
   lifecycle {
-    ignore_changes = [name, defined_tags]
+    ignore_changes = [defined_tags]
   }
 
   matching_rule = "ALL {instance.id = '${join(",", data.oci_core_instance.operator.*.id)}'}"
-  name          = "operator-instance-principal-${substr(uuid(), 0, 8)}"
+  name           = join("-", compact([
+    random_id.dynamic_group_suffix.keepers.label_prefix,
+    "operator-instance-principal",
+    random_id.dynamic_group_suffix.hex
+  ]))
 
   count = var.enable_operator_instance_principal == true ? 1 : 0
 }
 
-resource "oci_identity_policy" "enable_operator_instance_principal" {
+resource "oci_identity_policy" "operator_group_policy" {
   provider = oci.home
 
   compartment_id = var.compartment_id
   description    = "policy to allow operator host to call services"
-  name           = var.label_prefix == "none" ? "operator-instance-principal" : "${var.label_prefix}-operator-instance-principal"
-  statements     = ["Allow dynamic-group ${oci_identity_dynamic_group.enable_operator_instance_principal[0].name} to manage all-resources in compartment id ${var.compartment_id}"]
+  name           = join("-", compact([ local.dynamic_group_prefix, "operator-instance-principal" ]))
+  statements     = ["Allow dynamic-group ${oci_identity_dynamic_group.operator_group[0].name} to manage all-resources in compartment id ${var.compartment_id}"]
 
   count = var.enable_operator_instance_principal == true ? 1 : 0
 }
